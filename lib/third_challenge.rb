6d7e9ddaba23f4ee5cf328a2a502456385342397
Yana Ustsinovich <yustinovich@icloud.com>
2018-10-09 01:21:18 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,8 +26,9 @@ def third_challenge
         age: "51"
         },
       :heroine => {
-        name: "Lady Montague",
-        age: "54"
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
